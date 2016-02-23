b88db2791f041d806dbb13288039b86f2333209e
Arthur Zagretdinov <a.zagretdinov@outlook.com>
2016-02-23 10:03:54 +0300

    Init commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# travis-ci-mvn-multiprofile
---
+++ src/test/java/ProjectTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+/**
+ * @author Arthur Zagretdinov
+ * @date 23/02/16.
+ */
+public class ProjectTest {
+    
+    @Test
+    public void nothing() throws Exception {
+    }
+}
